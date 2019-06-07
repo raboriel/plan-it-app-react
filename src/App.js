3024dbf1b2d12f2d031728d38365c62ddd1fec56
Kyungsub Kim <raboriel@gmail.com>
2019-06-07 16:49:01 -0400

    add handleCreateTask

--- src/App.js
+++ src/App.js
@@ -7,14 +7,43 @@ import Form from './components/Form'
 class App extends Component {
   constructor(props) {
     super(props)
-    
+    this.state = {
+      listTasks: [],
+      likeCount: 0,
+      doneCount: 0
+    }
+    // Add binds below
+    this.handleCreateTask = this.handleCreateTask.bind(this)
+  }
+
+  // create a list from server
+  handleCreateTask(task) {
+    // add server address later
+    fetch('heroku server address here', {
+      body: JSON.stringify(task),
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(createdTask => {
+      return createdTask.json()
+    })
+    .then(jData => {
+      this.fetchTasks()
+    })
+    .catch(err => console.log(err))
   }
 
   render () {
     return (
       <div className="main-container">
         <Header />
-        <Form />
+
+        <Form
+          handleCreateTask={this.handleCreateTask}
+        />
         <Lists />
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react'
 
-
 class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      task_item: ''
+    }
+  }
+  // submit form
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.handleCreateTask(this.state)
+    this.clearForm()
+  }
+
+  //
+  handleChange = (e) => {
+    this.setState({task_item: e.target.value})
+  }
+
+  // clear the placeholder after submit
+  clearForm = () => {
+    this.setState({ task_item: '' })
+  }
 
   render () {
     return (
