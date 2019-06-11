4f4de78eec8702773ed1e1754ae98c1f8a715e34
Josh Ellison <joshellison@Jeremiahs-Mac-mini.local>
2019-06-11 12:14:14 -0700

    resolve conflict

--- src/components/TheList.js
+++ src/components/TheList.js
@@ -5,38 +5,8 @@ class TheList extends Component {
     console.log(this.props.listTasks);
     return (
       <div className="the-list">
-<<<<<<< HEAD
-        { this.props.currentView === 'list'
-           ? <div>
-             {this.props.listTasks.map( (list, index) => {
-               return (
-                 <Lists
-                   key={index}
-                   list={list}
-                   handleCheck={this.props.handleCheck}
-                   arrayIndex={index}
-                   currentArray='listTasks'
-                 />
-               )
-             })}
-             </div>
-           : <div>
-              {this.props.completedLists.map( (list, index) => {
-                return (
-                   <Lists
-                     key={index}
-                     list={list}
-                     handleCheck={this.props.handleCheck}
-                     arrayIndex={index}
-                     currentArray='completedLists'
-                   />
-                 )
-              })}
-             </div>
-         }
-=======
+
         <h4>{this.props.list}</h4>
->>>>>>> 8ca4afe6387a321cbc497ceaadb9a7389302291c
       </div>
    )
   }
